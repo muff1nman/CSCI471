4cb75f7e35cabd088a2a0b243cffe3a2bc6c8a9b
Andrew DeMaria <ademaria@mines.edu>
2013-09-13 14:46:40 -0600

    added socket as its own library

--- Projects/01/include/httpmuncher/socket/socket.h
+++ Projects/01/include/httpmuncher/socket/socket.h
@@ -25,8 +25,6 @@
 
 #include "httpmuncher/socket/echo_consumer.h"
 #include "httpmuncher/config.h"
-#include "httpmuncher/util/messages.h"
-#include "httpmuncher/util/sig_handle.h"
 
 #ifdef LOGGING
 #include <glog/logging.h>
@@ -36,121 +34,30 @@
 #include <iostream>
 #endif
 
-int global_fd = 0;
+static int global_fd = 0;
 
 /**
  * Opens a socket to listen on returning the socket fd
  */
-int open_listening_socket() {
-	return socket(AF_INET, SOCK_STREAM, 0);
-}
+int open_listening_socket();
 
 /**
  * Binds to a socket to a given port
  */
-int bind_listening_socket_to_port(int socket_fd, unsigned short port_number) {
-	struct sockaddr_in bind_opts;
-
-	// clear out the structure just in case
-	memset(&bind_opts, 0, sizeof(bind_opts));
-
-	bind_opts.sin_family = AF_INET;
-	bind_opts.sin_addr.s_addr = htonl(INADDR_ANY);
-	bind_opts.sin_port = htons(port_number);
-
-	return bind(socket_fd, (struct sockaddr*) &bind_opts, sizeof(bind_opts));
-}
+int bind_listening_socket_to_port(int socket_fd, unsigned short port_number);
 
 /**
  * Creates a listening socket and binds it to the given port number.  Returns
  * the listening socket file descriptor
  */
-int create_listening_tcp_port( unsigned short port_number ) {
-	int listening_fd = open_listening_socket();
-	if( listening_fd < SUCCESS ) {
-#ifdef LOGGING
-		LOG(FATAL) << ERROR_LISTENING_SOCKET << ": " << listening_fd << " [" << strerror(errno) << "]";
-#else
-		perror(ERROR_LISTENING_SOCKET);
-#endif
-		return listening_fd;
-	}
+int create_listening_tcp_port( unsigned short port_number );
 
-	int bind_status = bind_listening_socket_to_port( listening_fd, port_number );
-	if( bind_status < SUCCESS ) {
-#ifdef LOGGING
-		LOG(FATAL) << ERROR_BIND_SOCKET;
-#else
-		perror(ERROR_BIND_SOCKET);
-#endif
-		return bind_status; // we really just need a -1 value here
-	}
-
-	int listen_status = listen( listening_fd, BACKLOG );
-	if( listen_status < SUCCESS ) {
-#ifdef LOGGING
-		LOG(FATAL) << ERROR_LISTEN;
-#else
-		perror(ERROR_LISTEN);
-#endif
-		return listen_status; // we really just need a -1 value here
-	}
-
-	return listening_fd;
-}
-
-void handle_c( int sig ) {
-	int close_status = close(global_fd);
-	if(close_status < 0 ) {
-#ifdef LOGGING
-		LOG(ERROR) << "Could not close listening socket";
-#endif
-	} else {
-#ifdef LOGGING
-		LOG(INFO) << "Closed listening socket";
-#endif
-	}
-}
+void handle_c( int sig );
 
 // Runs a consumer and deletes it after completion.
-void thread_runner(Consumer* c) {
-	c->run();
-	delete c;
-}
+void thread_runner(Consumer* c);
 
-void accept_in_new_threads() {
-	int connection_fd;
-	int listen_fd = create_listening_tcp_port( LISTEN_PORT );
-	// we may assume listen_fd has already been checked...
-	if ( listen_fd < SUCCESS ) {
-		perror("error passed through create_listening_tcp_port...");
-	}
-
-	// save the global handler
-	global_fd = listen_fd;
-
-	// create a sigint handler
-	handle_sigint(handle_c);
-
-	while(true) {
-#ifdef LOGGING
-		LOG(INFO) << "Waiting for incoming requests on port " << LISTEN_PORT;
-#endif
-		connection_fd = accept(listen_fd, NULL ,NULL);
-		if( connection_fd < SUCCESS ) {
-#ifdef LOGGING
-			LOG(ERROR) << "Could not accept incoming request.. (quit)";
-#endif
-			break;
-		} else {
-#ifdef LOGGING
-			LOG(INFO) << "Accepted incoming request";
-#endif
-			boost::thread t(&thread_runner, new EchoConsumer(connection_fd));
-			t.detach();
-		}
-	}
-}
+void accept_in_new_threads();
 
 #endif /* !__socket_h__ */
 
