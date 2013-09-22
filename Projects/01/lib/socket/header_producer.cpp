eeb626e2c80f91108bebdb19f8df60df866b7f2e
Andrew DeMaria <ademaria@mines.edu>
2013-09-22 13:45:24 -0600

    Fix issue with valid files

--- Projects/01/lib/socket/header_producer.cpp
+++ Projects/01/lib/socket/header_producer.cpp
@@ -31,7 +31,15 @@ std::string HeaderProducer::get_path() {
 
 bool HeaderProducer::file_exists() {
 	std::string path = get_path();
-	return boost::filesystem::exists( path.c_str() );
+	bool exists = boost::filesystem::exists( path.c_str() ) && boost::filesystem::is_regular_file( path.c_str() );
+#ifdef LOGGING
+	if( exists ) {
+		LOG(INFO) << "File: " << path << " exists";
+	} else {
+		LOG(INFO) << "File: " << path << " does not exist";
+	}
+#endif
+	return exists;
 }
 
 std::string HeaderProducer::body_contents() {
