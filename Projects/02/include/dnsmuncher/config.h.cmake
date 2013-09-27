4a83bb04eb85dc0c3478eb19f2f363b6b0c2d0eb
Andrew DeMaria <ademaria@mines.edu>
2013-09-27 17:14:12 -0600

    Creating a basic executable

---
+++ Projects/02/include/dnsmuncher/config.h.cmake
@@ -0,0 +1,11 @@
+#ifndef __config_h__
+#define __config_h__
+
+#cmakedefine DEBUG
+#cmakedefine LOGGING
+
+#define LOG_LEVEL @LOG_LEVEL@
+
+#cmakedefine OLDBOOST
+
+#endif /* !__config_h__ */
---
+++ Projects/02/lib/main.cpp
@@ -0,0 +1,39 @@
+/*
+ * main.cpp
+ * Copyright (C) 2013 Andrew DeMaria (muff1nman) <ademaria@mines.edu>
+ *
+ * All Rights Reserved.
+ */
+
+#include "dnsmuncher/config.h"
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string print_usage( int argc, char** argv ) {
+	string use = 
+		"Usage: ./dnsmuncher";
+	return use;
+}
+
+string print_info( int argc, char** argv ) {
+	string info = std::string("info:\n");
+#ifdef DEBUG
+	info += "DEBUG ON\n";
+#endif
+#ifdef LOGGING
+	info += "LOGGING ON\n";
+	info += string("LOG LEVEL: ") + (char) ((int) '0' + LOG_LEVEL) + "\n";
+#endif
+	return info;
+}
+
+int main( int argc, char** argv ) {
+	cout << print_usage(argc, argv) << endl;
+	cout << print_info(argc, argv) << endl;
+	return 0;
+}
+
+
