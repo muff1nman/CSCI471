226cc7e6686dbf279cef74065c108a75f21864e9
Andrew DeMaria <ademaria@mines.edu>
2013-09-27 18:57:10 -0600

    Tests seem to be working

---
+++ Projects/02/test/random/random_test.cpp
@@ -0,0 +1,16 @@
+/*
+ * random_test.cpp
+ * Copyright (C) 2013 Andrew DeMaria (muff1nman) <ademaria@mines.edu>
+ *
+ * All Rights Reserved.
+ */
+
+
+#include "gtest/gtest.h"
+
+TEST(TestMethod, BlowsUp) {
+	EXPECT_EQ(0, 0);
+}
+
+
+
