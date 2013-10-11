9784d5873563abeb57fc5d3f1ee2cd5c9fda5a80
Andrew DeMaria <ademaria@mines.edu>
2013-10-11 16:30:48 -0600

    tests finished for byte util lib

--- Projects/02/include/dnsmuncher/util/byte.h
+++ Projects/02/include/dnsmuncher/util/byte.h
@@ -14,6 +14,8 @@
 
 #include "dnsmuncher/util/logging.h"
 
+#define BITS_PER_BYTE 8
+
 typedef unsigned char Byte;
 typedef boost::shared_ptr<Byte> Bytes;
 
@@ -31,58 +33,23 @@ std::bitset<N1 + N2> concat( const std::bitset<N1> & b1, const std::bitset<N2> &
 	return std::bitset <N1 + N2>( s1 + s2 );
 }
 
-template <size_t n>
-BytesContainer convert_big_endian( const std::bitset<8 * n>& bits ) {
-	BytesContainer toReturn;
-	toReturn.data = Bytes( new Byte[n] );
-	toReturn.size = n;
-
-	for( size_t i = 0; i < n; ++i ) {
-		toReturn.data.get()[i] = convert_to_char_big_endian( bits, i );
-	}
-
-	return toReturn;
-}
-
-template <size_t n>
-BytesContainer convert_little_endian( const std::bitset<8 * n>& bits ) {
-	BytesContainer toReturn;
-	toReturn.data = Bytes( new Byte[n] );
-	toReturn.size = n;
-
-	for( size_t i = 0; i < n; ++i ) {
-		toReturn.data.get()[i] = convert_to_char_little_endian( bits, i );
-	}
-
-	return toReturn;
-}
-
-template <size_t width, size_t n>
-BytesContainer convert_to_char_big_endian( const std::bitset<8 * n>& bits, size_t index = 0 ) {
-	// TODO refactor common code
-	if ( n < width ) {
-		Logging::do_error("Width too large");
-	}
-
-	if ( n/width <= index ) {
-		Logging::do_error("Out of Bounds");
-	}
-	//  TODO
-	//return convert_to_char_little_endian<w>( bits, n / width - index );
-}
-
 /**
+ * Given an ordered stream of bits, returns the byte at a given offset.  
+ *
+ * For Little Endian, this offset starts at least significant set of bits (the
+ * right most bit on a bit stream)
+ *
  * For example with the following call
  * convert_to_char_little_endian<3,6>( std::string("110100"), 1 );
  *
  * index would count starting at the far right (index zero) and land at the 3rd
  * zero from the right to point at  "110"
  *
- * and 6 would be returned (in char form)
+ * and 6 would be returned (in Byte form)
  *
  */
 template <size_t width, size_t n>
-unsigned char convert_to_char_little_endian( const std::bitset<n>& bits, size_t index = 0 ) {
+Byte convert_to_char_little_endian( const std::bitset<n>& bits, size_t index = 0 ) {
 
 	if ( n < width ) {
 		Logging::do_error("Width too large");
@@ -95,6 +62,71 @@ unsigned char convert_to_char_little_endian( const std::bitset<n>& bits, size_t 
 	return (unsigned char) ((bits << ( n/width - (index + 1) ) * width) >> ((n/width-1)*width)).to_ulong();
 }
 
+
+/**
+ * Given an ordered stream of bits, returns the byte at a given offset.  
+ *
+ * For Big Endian, this offset starts at most significant set of bits (the left
+ * most bit on a bit stream)
+ *
+ * For example with the following call
+ * convert_to_char_big_endia<3,6>( std::string("110100"), 1);
+ *
+ * index would count starting at the far left (index zero) and land at the 3rd
+ * one from the left to point at "100"
+ *
+ * and would return 4 (in Byte form)
+ */
+template <size_t width, size_t n>
+Byte convert_to_char_big_endian( const std::bitset<n>& bits, size_t index = 0 ) {
+	// TODO refactor common code
+	if ( n < width ) {
+		Logging::do_error("Width too large");
+	}
+
+	if ( n/width <= index ) {
+		Logging::do_error("Out of Bounds");
+	}
+	return convert_to_char_little_endian<width>( bits, n / width - index - 1 );
+}
+
+/**
+ * Given an ordered set of bits, return a set of Bytes to resemble those bits.
+ * The size of the bit stream must be evenly divisible into groups of
+ * BITS_PER_BYTE. For the big endian version, the first byte in the returned
+ * data corresponds to the most significant (leftmost) byte in the given bit stream.
+ */
+template <size_t n>
+BytesContainer convert_big_endian( const std::bitset<BITS_PER_BYTE * n>& bits ) {
+	BytesContainer toReturn;
+	toReturn.data = Bytes( new Byte[n] );
+	toReturn.size = n;
+
+	for( size_t i = 0; i < n; ++i ) {
+		toReturn.data.get()[i] = convert_to_char_big_endian<BITS_PER_BYTE>( bits, i );
+	}
+
+	return toReturn;
+}
+
+/**
+ * Given an ordered set of bits, return a set of Bytes to resemble those bits.
+ * The size of the bit stream must be evenly divisible into groups of
+ * BITS_PER_BYTE. For the little endian version, the first byte in the returned
+ * data corresponds to the least significant (rightmost) byte in the given bit stream.
+ */
+template <size_t n>
+BytesContainer convert_little_endian( const std::bitset<BITS_PER_BYTE * n>& bits ) {
+	BytesContainer toReturn;
+	toReturn.data = Bytes( new Byte[n] );
+	toReturn.size = n;
+
+	for( size_t i = 0; i < n; ++i ) {
+		toReturn.data.get()[i] = convert_to_char_little_endian<BITS_PER_BYTE>( bits, i );
+	}
+
+	return toReturn;
+}
 
 #endif /* !__byte_h__ */
 
--- Projects/02/test/dnsmuncher/util/util_test.cpp
+++ Projects/02/test/dnsmuncher/util/util_test.cpp
@@ -15,12 +15,28 @@
 #include <bitset>
 #include <string>
 
+TEST(ByteListTest, MultipleWidthBitset) {
+	const char* same_order = "\xa5\x51";
+	const char* flip_order = "\x51\xa5";
+
+	// in decimal: 165,81; in hex: a5,51
+	std::bitset<16> t1(std::string("1010010101010001"));
+	BytesContainer a = convert_big_endian<2>(t1);
+	EXPECT_EQ(2, a.size);
+	EXPECT_TRUE( 0 == memcmp( same_order, a.data.get(), a.size)) << "Convert big endian out of order";
+	a = convert_little_endian<2>(t1);
+	EXPECT_EQ(2, a.size);
+	EXPECT_TRUE( 0 == memcmp( flip_order, a.data.get(), a.size)) << "Convert little endian out of order";
+}
+
 TEST(ByteTest, SingleWidthBitset) {
 	std::bitset<8> t1(1);
 	EXPECT_EQ(t1.to_ulong(), (unsigned long) convert_to_char_little_endian<8>(t1)) << "Char does not hold a single bit";
+	EXPECT_EQ(t1.to_ulong(), (unsigned long) convert_to_char_big_endian<8>(t1)) << "Char does not hold a single bit";
 
 	t1 = std::bitset<8>(std::string("11111111"));
 	EXPECT_EQ(255, (unsigned long) convert_to_char_little_endian<8>(t1)) << "Returned character is not set with every bit";
+	EXPECT_EQ(255, (unsigned long) convert_to_char_big_endian<8>(t1)) << "Returned character is not set with every bit";
 }
 
 TEST(ByteTest, MultipleWidthBitset) {
@@ -28,9 +44,15 @@ TEST(ByteTest, MultipleWidthBitset) {
 	EXPECT_EQ(6, (unsigned long) convert_to_char_little_endian<3>( t1, 1 ));
 	EXPECT_EQ(4, (unsigned long) convert_to_char_little_endian<3>( t1, 0));
 
+	EXPECT_EQ(6, (unsigned long) convert_to_char_big_endian<3>( t1, 0 ));
+	EXPECT_EQ(4, (unsigned long) convert_to_char_big_endian<3>( t1, 1 ));
+
 	std::bitset<16> t2(std::string("0101110110000110"));
 	EXPECT_EQ(93, (unsigned long) convert_to_char_little_endian<8>( t2, 1));
 	EXPECT_EQ(134, (unsigned long) convert_to_char_little_endian<8>( t2, 0));
+
+	EXPECT_EQ(93, (unsigned long) convert_to_char_big_endian<8>( t2, 0));
+	EXPECT_EQ(134, (unsigned long) convert_to_char_big_endian<8>( t2, 1));
 
 }
 
@@ -38,6 +60,9 @@ TEST(ByteTest, InvalidParams) {
 	std::bitset<4> t1(std::string("1000"));
 	EXPECT_DEATH({ convert_to_char_little_endian<2>( t1, 3 );}, "Out of Bounds");
 	EXPECT_DEATH({ convert_to_char_little_endian<5>( t1, 1 );}, "Width too large");
+
+	EXPECT_DEATH({ convert_to_char_big_endian<2>( t1, 3 );}, "Out of Bounds");
+	EXPECT_DEATH({ convert_to_char_big_endian<5>( t1, 1 );}, "Width too large");
 
 }
 
