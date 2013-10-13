004faf03c8e14382058d3ab1c1291b9753747ecd
Andrew DeMaria <ademaria@mines.edu>
2013-10-13 15:26:53 -0600

    Refactored resources dir

--- Projects/02/test/include/file_names.h.cmake
+++ Projects/02/test/include/file_names.h.cmake
@@ -8,7 +8,7 @@
 #ifndef __file_names_h__
 #define __file_names_h__
 
-#define QUERY_A_GOOGLE "${CMAKE_CURRENT_BINARY_DIR}/query_a_google"
+#define QUERY_A_GOOGLE "${RESOURCES_OUTPUT_DIR}/query_a_google"
 
 #endif /* !__file_names_h__ */
 
